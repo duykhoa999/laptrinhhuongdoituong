9a2d2d1a734ed0ab3b033dc1357d82dbeb26f693
Duy Khoa <duykhoaptit@gmail.com>
2022-10-11 22:31:41 +0700

    Temp

--- src/main/java/com/group28/laptrinhhuongdoituong/converter/CategoryConverter.java
+++ src/main/java/com/group28/laptrinhhuongdoituong/converter/CategoryConverter.java
@@ -1,27 +1,2 @@
-package com.group28.laptrinhhuongdoituong.converter;
-
-import com.group28.laptrinhhuongdoituong.dto.CategoryDTO;
-import com.group28.laptrinhhuongdoituong.entity.CategoryEntity;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-@Component
-public class CategoryConverter {
-//    @Autowired
-//    private final ModelMapper mapper;
-//
-//
-//    public CategoryConverter(ModelMapper mapper) {
-//        this.mapper = mapper;
-//    }
-//
-//    public CategoryEntity toEntity(CategoryDTO category){
-//        CategoryEntity categoryEntity = mapper.map(category, CategoryEntity.class);
-//        return categoryEntity;
-//    }
-//
-//    public CategoryDTO toDTO(CategoryEntity category){
-//        CategoryDTO categoryDTO = mapper.map(category, CategoryDTO.class);
-//        return categoryDTO;
-//    }
+package com.group28.laptrinhhuongdoituong.converter;public class CategoryConverter {
 }
