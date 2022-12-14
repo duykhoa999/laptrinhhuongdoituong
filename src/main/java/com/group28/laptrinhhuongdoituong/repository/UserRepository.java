7d44af4543bea3c151210372b41972b03cc06dc0
Vo Le Quoc Dung <volequocdung5699@gmail.com>
2022-12-14 07:35:32 +0700

    fix user

--- src/main/java/com/group28/laptrinhhuongdoituong/repository/UserRepository.java
+++ src/main/java/com/group28/laptrinhhuongdoituong/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.group28.laptrinhhuongdoituong.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -10,6 +11,7 @@ import com.group28.laptrinhhuongdoituong.entity.UserEntity;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
-  @Query(value = "SELECT * FROM users u, roles r WHERE u.role_id = r.id and CONCAT(u.first_name,' ',u.last_name,' ',u.gender,' ',u.dob,' ',u.address,' ',u.phone,' ',u.email,' ',c.password,' ',r.name) LIKE %?%", nativeQuery=true)
+  @Query(value = "SELECT * FROM users u, roles r WHERE u.role_id = r.id and CONCAT(u.first_name,' ',u.last_name,' ',u.gender,' ',u.dob,' ',u.address,' ',u.phone,' ',u.email,' ',r.name) LIKE %?%", nativeQuery=true)
   public List<UserEntity> search(String keyword);
+  Optional<UserEntity> findByEmail(String email);
 }
